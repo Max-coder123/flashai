932a9db8c33318041c58fb3a67181b0de1216e14
Max <maxx68329@gmail.com>
2024-07-27 10:21:22 -0700

    add flask

--- app.py
+++ app.py
@@ -1,45 +1,35 @@
-import json
 import os
 import requests
+import sys
 from dotenv import load_dotenv
-from pyfiglet import Figlet
+from flask import Flask, request, render_template
 
 load_dotenv()  # take environment variables from .env.
+if not os.environ.get('OPENAI_API_KEY'):
+    print("OPENAI_API_KEY not found in env")
+    sys.exit(1)
 
 
-f = Figlet(font="varsity")
-fonts = f.getFonts()
-print(f.renderText("Max's Chat"))
+app = Flask(__name__)
 
-
-messages = []
-while True:
-    question = input("> ")
-    messages.append({"role": "user", "content": question})
+@app.route('/', methods = ["GET", "POST"])
+def index():
+    content = request.form.get("question", request.args.get("question","hello"))
+    messages = [{"role": "user", "content": content}]
     headers = {"Authorization": f"Bearer {os.environ.get('OPENAI_API_KEY')}"}
     url = "https://api.openai.com/v1/chat/completions"
     data = {
-        "model": "gpt-4-turbo",
+        "model": "gpt-4o-mini",
         "messages": messages,
         "temperature": 0.5,
         "max_tokens": 1000,
-        "stream": True,  # Enable streaming
     }
 
     response = requests.post(url, headers=headers, json=data, stream=True)
     response.raise_for_status()
+    content = response.json()["choices"][0]["message"]["content"]
+    return render_template("index.html",content=content)
 
-    ai_response = ""
-    for line in response.iter_lines():
-        if line:
-            decoded_line = line.decode("utf-8")
-            if decoded_line.startswith("data: "):
-                try:
-                    json_data = json.loads(decoded_line[len("data: ") :])
-                    content = json_data["choices"][0]["delta"].get("content", "")
-                    ai_response += content
-                    print(content, end="", flush=True)
-                except json.JSONDecodeError:
-                    continue
-    print()  # To ensure the final output ends with a newline
-    messages.append({"role": "assistant", "content": ai_response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
